7d2276eba86e1ab0a4edf8d7147a310017b769d0
tomatolian <vinegtom@gmail.com>
2022-05-04 19:47:34 +0900

    make home page

--- js/script.js
+++ js/script.js
@@ -4,20 +4,6 @@ function setHeight() {
   }
   setHeight();
   window.addEventListener("resize", setHeight);
-
-  const verticalSlider = new Swiper(".vertical-slider", {
-    direction: "vertical",
-    slidesPerView: 1,
-    speed: 600,
-    effect: "fade",
-    mousewheel: true,
-    pagination: {
-      el: ".vertical-slider__pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-  
 $(function () {
   $('a[href^="#"]').on("click", function () {
     var href = $(this).attr("href");
