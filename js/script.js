ec98c9b898ce865a4c8c7f928e1a17ef3f8463b8
tomatolian <vinegtom@gmail.com>
2022-05-05 00:19:33 +0900

    add news section

--- js/script.js
+++ js/script.js
@@ -4,6 +4,19 @@ function setHeight() {
   }
   setHeight();
   window.addEventListener("resize", setHeight);
+  $.ajax({
+		url: "https://script.google.com/macros/s/AKfycbzRJd7yi1dSgJJwS_4k8B_Z7gO8-KF_G7vsmDPMELeVZKJNipMZ19_p6nbYAU_o1NkX/exec"
+	}).done(function(data){ 
+    for (let key in data){
+      console.log(key)
+      title=data[key]['title'];
+      date=data[key]['date'];
+      img_path=data[key]['img-path'];
+      text=data[key]['text'];
+      console.log(img_path)
+      $('.news-wrapper').append("<div class='swiper-slide news-slide'><div class='card'><img src='img/campus0.png'/><div class='card-body'><h5 class='card-title'>"+title+"</h5><p class='card-text'>"+text+"</p></div></div></div>")
+    }
+  })
 $(function () {
   $('a[href^="#"]').on("click", function () {
     var href = $(this).attr("href");
