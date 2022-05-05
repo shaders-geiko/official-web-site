c32235621e6091bd54109739a25f65e86b3afa4c
tomatolian <vinegtom@gmail.com>
2022-05-05 16:32:27 +0900

    fix js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-function setHeight() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-  }
-  setHeight();
-  window.addEventListener("resize", setHeight);
+$(function () {
+  var navHeight = $(".header").outerHeight();
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    console.log(position);
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
   $.ajax({
 		url: "https://script.google.com/macros/s/AKfycbzRJd7yi1dSgJJwS_4k8B_Z7gO8-KF_G7vsmDPMELeVZKJNipMZ19_p6nbYAU_o1NkX/exec"
 	}).done(function(data){ 
@@ -17,13 +21,5 @@ function setHeight() {
       $('.news-wrapper').append("<div class='swiper-slide news-slide'><div class='card'><img src='img/campus0.png'/><div class='card-body'><h5 class='card-title'>"+title+"</h5><p class='card-text'>"+text+"</p></div></div></div>")
     }
   })
-$(function () {
-  $('a[href^="#"]').on("click", function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top - navHeight;
-    $("html, body").animate({ scrollTop: position, }, 300, "swing");
-    return false;
-  });
 
 })
