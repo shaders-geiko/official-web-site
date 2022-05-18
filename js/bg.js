ce59a34c5dbaf7e8fb166380cd51f11d29451f5f
tomatolian <vinegtom@gmail.com>
2022-05-19 07:10:24 +0900

    rename 3dmodel

--- js/bg.js
+++ js/bg.js
@@ -27,7 +27,8 @@ function init() {
 
   new THREE.GLTFLoader()
    .setPath( 'models/' )
-   .load( 'GAKUMU_test_001.glb', function ( gltf ) {
+   .load( 'gakumu.glb', function ( gltf ) {
+     console.log('loading now')
     flower=gltf.scene
     flower.scale.set(10,10,10)
     scene.add(flower);
