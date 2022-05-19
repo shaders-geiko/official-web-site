0bc9bf69f6202bd1e7fd89d9bd4476e37a12d0a8
tomatolian <vinegtom@gmail.com>
2022-05-19 15:24:05 +0900

    test

--- js/bg.js
+++ js/bg.js
@@ -24,7 +24,7 @@ function init() {
   );
   camera.position.set(0, 0, +200);
   var loader = new THREE.GLTFLoader();
-  var dracoLoader = new DRACOLoader();
+  var dracoLoader = new THREE.DRACOLoader();
   dracoLoader.setDecoderPath( 'js/draco/' );
   dracoLoader.setDecoderConfig({type: 'js'});
   loader.setDRACOLoader( new dracoLoader);
